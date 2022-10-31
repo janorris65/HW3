0c2c6a4b1632fb60ce5f187579a9a3ef629d0452
Joshua <janorris65@gmail.com>
2022-10-31 00:25:14 -0500

    modifying Readme and 'guarantee' function

--- script.js
+++ script.js
@@ -104,38 +104,66 @@ function generatePassword() {
   let typesymbols = confirm(
     "Would you like symbols in your password? Ok for Yes, Cancel for No"
   );
-  // These set the conditions for array concat using prompt confirm
+  // These set the conditions for array concat using prompt confirm. q,r,s,t correct for 'No' implication in oneOfEach function
+
+  let q, r, s, t;
   let x = [];
   if (typelower === true) {
     x = lowerCasedCharacters;
+    q = 0;
+  } else {
+    q = 1;
   }
   let y = [];
   if (typenum === true) {
     y = numericCharacters;
+    r = 0;
+  } else {
+    r = 1;
   }
   let z = [];
   if (typesymbols === true) {
     z = specialCharacters;
+    s = 0;
+  } else {
+    s = 1;
   }
   let p = [];
   if (typeupper === true) {
     p = upperCasedCharacters;
+    t = 0;
+  } else {
+    t = 1;
   }
+  let numBe = Number(num) + q + r + s + t;
   const Allarray = x.concat(y, z, p);
   //Correction of effect of using linear "for" loop method to create string array on random generator using "date" and "i+5"
   let date = Date.now() % 1000;
 
   console.log(date);
-
+  // if statement for outside 8 and 128
   if (num < 8 || num > 128) {
     return "Must be between 8 and 128";
   }
+  //block of code for creating random array
   let apples = [];
-  for (let i = 1; num >= i; i++) {
+  for (let i = 1; numBe >= i; i++) {
     let lettersoutput =
       Allarray[(date * Math.floor(Math.random() * (i + 5))) % Allarray.length];
     apples.push(lettersoutput);
   }
+  // function to guarantee one of each selected in code
+  function oneOfEach(Fruit) {
+    Fruit[Math.floor(Math.random() * apples.length)] =
+      x[Math.floor(Math.random() * upperCasedCharacters.length)];
+    Fruit[Math.floor(Math.random() * apples.length)] =
+      y[Math.floor(Math.random() * numericCharacters.length)];
+    Fruit[Math.floor(Math.random() * apples.length)] =
+      z[Math.floor(Math.random() * specialCharacters.length)];
+    Fruit[Math.floor(Math.random() * apples.length)] =
+      p[Math.floor(Math.random() * upperCasedCharacters.length)];
+  }
+  oneOfEach(apples);
   return apples.join("");
 }
 
